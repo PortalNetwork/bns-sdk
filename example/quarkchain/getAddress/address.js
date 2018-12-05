a3c9a0edc5ee3a533469df0d06bbc1cd8242bb06
billPortalNetwork <bill@portal.network>
2018-12-05 14:39:12 +0800

    Complete intrgration tests

--- example/quarkchain/getAddress/address.js
+++ example/quarkchain/getAddress/address.js
@@ -15,4 +15,3 @@ async function getAddress() {
 }
 
 getAddress();
-
--- src/helper/qcns/registryService.js
+++ src/helper/qcns/registryService.js
@@ -10,9 +10,9 @@ let registry = null;
 const getQcnsRegistryAddress = (networkId = '1') => {
   switch (networkId) {
     case '1':
-      return '0x9799D981FC2e0445aEeF29F6533b199bB7650f2cf05d8fc2';
+      return '0x4b94640a5503f59f5A7a263769eFC1a4692CCD03f05d8fc2';
     case '3':
-      return '0x9799D981FC2e0445aEeF29F6533b199bB7650f2cf05d8fc2';
+      return '0x4b94640a5503f59f5A7a263769eFC1a4692CCD03f05d8fc2';
     default:
       return '0x0';
   }
